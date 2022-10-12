0e91c899b60406265746c1e0bc697a0f00865ad7
Nicholas Lal <nlalkta@gmail.com>
2022-10-11 21:13:04 -0600

    -cleanup🧹 -worked on installing test dependencies 🧪

---
+++ tests/tests.test.js
@@ -0,0 +1,9 @@
+import { trimAndLowerCase } from '../scripts/utils';
+
+const { expect } = require('chai');
+
+describe ('trimAndLowerCase function', () => {
+  it('should return a string in lowercase with it\'s whitespace removed', () => {
+    console.log('trying to test...')
+  })
+})
